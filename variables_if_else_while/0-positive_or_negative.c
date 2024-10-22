17e38febcca047d03cdfd8b06bfa9c3718ed021f
wwalid18 <9833@holbertonstudents.com>
2024-10-22 12:07:57 +0100

    still same bug

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,9 +8,10 @@ int main(void)
 {
 	int n;
 
-	n=98;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 if (n > 0)
-{printf("%d is positive \n",n);
+{printf("%d is positive\n",n);
 }
 else if  (n == 0)
 {printf("%d is zero\n",n);
