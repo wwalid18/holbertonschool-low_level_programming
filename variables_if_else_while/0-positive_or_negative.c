a3f617bf0e9cfaf3f753497237c01b4f0095d013
wwalid18 <9833@holbertonstudents.com>
2024-10-22 11:41:01 +0100

    0-positive_or_negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,13 +11,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 if (n == 0)
-{puts("%i is zero",n);
+{printf("%d is zero",n);
 }
 else if (n > 0)
-{puts("%i is positive",n);
+{printf("%d is positive",n);
 }
 else
-{puts("%i is negative",n);
+{printf("%d is negative",n);
 }
 return (0);
 }
