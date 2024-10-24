856af2f61959773f679d19c2c20b6c319cb881d8
wwalid18 <9833@holbertonstudents.com>
2024-10-24 14:27:28 +0100

    2-alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+int i, j;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 26; i++)
+{
+_putchar(alphabet[i]);
+}
+_putchar('\n');
+}
+}
