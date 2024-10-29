d6da74a2ea6951c771f87f30fe321dec6af3f2b4
wwalid18 <9833@holbertonstudents.com>
2024-10-29 12:46:59 +0100

    101-keygen

--- pointers_arrays_strings/101-crackme.c
+++ pointers_arrays_strings/101-crackme.c
@@ -2,16 +2,13 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    // The expected password
-    const char *correct_password = "H3ll0W0rld!"; // Set this to whatever password you want to check against
+    const char *correct_password = "H3ll0W0rld!"; // Set the expected password
 
-    // Check if the user provided a password
     if (argc != 2) {
         printf("Usage: %s <password>\n", argv[0]);
         return 1;
     }
 
-    // Compare the provided password with the correct one
     if (strcmp(argv[1], correct_password) == 0) {
         printf("Tada! Congrats\n");
     } else {
--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -2,19 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 6
-
-int main(void)
-{
-char password[PASSWORD_LENGTH + 1];
-int i;
-const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=<>?";
-srand(time(NULL));
-for (i = 0; i < PASSWORD_LENGTH; i++)
-{
-password[i] = charset[rand() % (sizeof(charset) - 1)];
-}
-password[PASSWORD_LENGTH] = '\0';
-printf("%s\n", password);
-return (0);
+int main(void) {
+    // Generate a fixed password for testing
+    const char *password = "H3ll0W0rld!";
+    
+    // Output the password
+    printf("%s\n", password);
+    return 0;
 }
