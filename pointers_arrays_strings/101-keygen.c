348a9541bd78337ea8c75d55dbab6eb81973eef6
wwalid18 <9833@holbertonstudents.com>
2024-10-29 12:47:53 +0100

    i ruined what ivdone

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void) {
-    // Generate a fixed password for testing
-    const char *password = "H3ll0W0rld!";
-    
-    // Output the password
-    printf("%s\n", password);
-    return 0;
+#define PASSWORD_LENGTH 6
+
+int main(void)
+{
+char password[PASSWORD_LENGTH + 1];
+int i;
+const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=<>?";
+srand(time(NULL));
+for (i = 0; i < PASSWORD_LENGTH; i++)
+{
+password[i] = charset[rand() % (sizeof(charset) - 1)];
+}
+password[PASSWORD_LENGTH] = '\0';
+printf("%s\n", password);
+return (0);
 }
