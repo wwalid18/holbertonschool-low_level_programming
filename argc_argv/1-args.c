d3214aefd1cd7396a6a80e84d9daa7f7943a193f
wwalid18 <9833@holbertonstudents.com>
2024-11-07 15:36:14 +0100

    nargs

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector (array of command line arguments, not used)
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
