b3b1c2ff3314e35b9fd00de8532193ebd51d34e2
wwalid18 <9833@holbertonstudents.com>
2024-11-07 15:33:39 +0100

    mynewnameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count (not used here)
+ * @argv: Argument vector (array of command line arguments)
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
