c0399e1ff45d6bae41eee5ed81fa64f8b39de6cb
wwalid18 <9833@holbertonstudents.com>
2024-11-15 13:48:21 +0100

    more doggo stuff

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -6,15 +6,15 @@
  */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        return;
-    if (d->name == NULL)
-        printf("Name: (nil)\n");
-    else
-        printf("Name: %s\n", d->name);
-    printf("Age: %.6f\n", d->age);
-    if (d->owner == NULL)
-        printf("Owner: (nil)\n");
-    else
-        printf("Owner: %s\n", d->owner);
+if (d == NULL)
+return();
+if (d->name == NULL)
+printf("Name: (nil)\n");
+else
+printf("Name: %s\n", d->name);
+printf("Age: %.6f\n", d->age);
+if (d->owner == NULL)
+printf("Owner: (nil)\n");
+else
+printf("Owner: %s\n", d->owner);
 }
