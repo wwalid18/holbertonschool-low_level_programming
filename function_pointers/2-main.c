56b738c5d52f84d3efed9dc8a20b40c6479c4348
wwalid18 <9833@holbertonstudents.com>
2024-11-21 22:51:02 +0100

    task 3 of function pointers

--- function_pointers/2-main.c
+++ function_pointers/2-main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "function_pointers.h"
+#include "calc.h"
 
 /**
  * is_98 - check if a number is equal to 98
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,70 @@
+#include "calc.h"
+
+/**
+ * op_add - calc the sum of a and b.
+ * @a: int 1.
+ * @b: int 2.
+ * Return: the add.
+ */
+int op_add(int a, int b)
+{
+int r;
+r = a + b;
+return (r);
+}
+/**
+ * op_sub - calc the difference of a and b.
+ * @a: int 1.
+ * @b: int 2.
+ * Return: the sub.
+ */
+int op_sub(int a, int b)
+{
+int r;
+r = a - b;
+return (r);
+}
+/**
+ * op_mul - calc the product of a and b.
+ * @a: int 1.
+ * @b: int 2.
+ * Return: the mul.
+ */
+int op_mul(int a, int b)
+{
+int r;
+r = a * b;
+return (r);
+}
+/**
+ * op_div - calc the result of the division of a by b.
+ * @a: int 1.
+ * @b: int 2.
+ * Return: the div.
+ */
+int op_div(int a, int b)
+{
+int r;
+if (b == 0)
+{
+return (0);
+}
+r = a / b;
+return (r);
+}
+/**
+ * op_mod - calc the remainder of the division of a by b.
+ * @a: int 1.
+ * @b: int 2.
+ * Return: the mod.
+ */
+int op_mod(int a, int b)
+{
+int r;
+if (b == 0)
+{
+return (0);
+}
+r = a % b;
+return (r);
+}
