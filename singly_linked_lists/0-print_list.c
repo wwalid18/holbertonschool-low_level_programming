fd13acd0c8a21fd11e7973bdbc04033c91227016
wwalid18 <9833@holbertonstudents.com>
2024-12-04 12:26:34 +0100

    i overthinked it

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,37 +8,19 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t x = 0;
-char digits[10];
-int j = 0, i = 0;
-unsigned int number = h->len;
+size_t i = 0;
 
 while (h != NULL)
 {
 	if (h->str == NULL)
-		write(1, "[0] (nil)", 10);
-	else
 	{
-		_putchar('[');
-
-		while (number > 0)
-		{
-			digits[i++] = (number % 10) + '0';
-			number /= 10;
-		}
-		for (j = i - 1; j >= 0; j--)
-		{
-			_putchar(digits[j]);
-		}
-		write(1, "] ", 2);
-		for (i = 0; h->str[i] != '\0'; i++)
-		{
-			_putchar(h->str[i]);
-		}
-		_putchar('\n');
+		write(1, "[0] (nil)", 10);
+		printf("\n");
 	}
+	else
+		printf("[%u] %s\n", h->len, h->str);
 	h = h->next;
-	x++;
+	i++;
 }
-return (x);
+return (i);
 }
