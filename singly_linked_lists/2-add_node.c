22f1f2f7015b9901ab1ab0051c145a1b61959194
wwalid18 <9833@holbertonstudents.com>
2024-12-05 11:44:11 +0100

    i figured it out

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,15 +13,17 @@ list_t *new_node;
 char *str2;
 unsigned int len2 = 0;
 str2 = strdup(str);
-new_node = malloc(sizeof(list_t));
-new_node->str = str2;
 if (str == NULL)
 return (NULL);
+if (str2 == NULL)
+return (NULL);
+new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 {
-free(new_node);
+free(str2);
 return (NULL);
 }
+new_node->str = str2;
 while (str[len2])
 len2++;
 new_node->len = len2;
