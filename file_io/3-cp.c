9b12fae9978cef6d1795e09f23c4f59111546947
wwalid18 <9833@holbertonstudents.com>
2024-12-08 18:56:36 +0100

    last task

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -24,14 +24,14 @@ void print_fd_error(const char *msg, int fd_value, int exit_code)
 }
 
 /**
- * print_name_error - Prints an error message to the standard error
+ * print_error - Prints an error message to the standard error
  * @msg: The message to print
- * @file_value: The file name or descriptor causing the error
+ * @arg: The file name or descriptor causing the error
  * @exit_code: The exit code to return
  */
-void print_name_error(const char *msg, const char *file_value, int exit_code)
+void print_error(const char *msg, const char *arg, int exit_code)
 {
-	dprintf(STDERR_FILENO, msg, file_value);
+	dprintf(STDERR_FILENO, msg, arg);
 	exit(exit_code);
 }
 
@@ -49,26 +49,26 @@ int main(int argc, char **argv)
 	ssize_t nread, bw;
 
 	if (argc != 3)
-		print_name_error("Usage: cp file_from file_to\n", NULL, 97);
+		print_error("Usage: cp file_from file_to\n", NULL, 97);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		print_name_error("Error: Can't read from file %s\n", argv[1], 98);
+		print_error("Error: Can't read from file %s\n", argv[1], 98);
 	umask(0000);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-		print_name_error("Error: Can't write to file %s\n", argv[2], 99);
+		print_error("Error: Can't write to file %s\n", argv[2], 99);
 	while ((nread = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		bw = write(fd_to, buffer, nread);
 		if (bw == -1)
-			print_name_error("Error: Can't write to file %s\n", argv[2], 99);
+			print_error("Error: Can't write to file %s\n", argv[2], 99);
 	}
 	if (nread == -1)
-		print_name_error("Error: Can't read from file %s\n", argv[1], 98);
+		print_error("Error: Can't read from file %s\n", argv[1], 98);
 	if (close(fd_from) == -1)
-		print_fd_error("Error: Can't close fd %d\n", fd_from, 100);
+		print_error("Error: Can't close fd %d\n", "", 100);
 	if (close(fd_to) == -1)
-		print_fd_error("Error: Can't close fd %d\n", fd_to, 100);
+		print_error("Error: Can't close fd %d\n", "", 100);
 	return (0);
 }
 
